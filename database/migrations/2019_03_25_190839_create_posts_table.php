e732e274917812840071189aa518589fac6a70dd
Asmaa <asmaaali2017@gmail.com>
2019-03-27 09:57:43 +0200

    Asmaa edited the body field in DB

--- database/migrations/2019_03_25_190839_create_posts_table.php
+++ database/migrations/2019_03_25_190839_create_posts_table.php
@@ -17,7 +17,7 @@ class CreatePostsTable extends Migration
             $table->increments('id');            
             $table->timestamps();
             $table->string('title');
-            $table->string('body');
+            $table->text('body');
         });
     }
 
